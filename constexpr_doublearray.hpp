bb6175a0c6b13c4d599b189aa1586f0727799805
Shunsuke Kanda <shnsk.knd@gmail.com>
2021-05-14 08:29:45 +0900

    update

--- constexpr_doublearray.hpp
+++ constexpr_doublearray.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <stdexcept>
 #include <string_view>
 
@@ -38,7 +39,7 @@ class unit {
     }
 };
 
-namespace details {
+namespace utils {
 
 // Constexpr version of algorithm::find
 template <class Iterator, class Value>
@@ -48,120 +49,6 @@ constexpr Iterator find(Iterator first, Iterator last, const Value& value) {
     }
     return first;
 }
-
-template <class Words>
-constexpr void get_num_nodes(const Words& words, std::size_t& num_nodes,  //
-                             std::size_t bpos, std::size_t epos, std::size_t depth) {
-    if (std::size(words[bpos]) == depth) {
-        return;
-    }
-
-    auto i = bpos;
-    auto c1 = words[bpos][depth];
-
-    for (auto j = bpos + 1; j < epos; j++) {
-        const auto c2 = words[j][depth];
-        if (c1 != c2) {
-            get_num_nodes(words, ++num_nodes, i, j, depth + 1);
-            i = j;
-            c1 = c2;
-        }
-    }
-    get_num_nodes(words, ++num_nodes, i, epos, depth + 1);
-}
-
-template <class Words, class Units>
-class builder {
-  public:
-    using int_type = typename Units::value_type::int_type;
-    using char_type = typename Words::value_type::value_type;
-
-  private:
-    const Words& m_words;
-    Units m_units;
-
-    int_type m_id = 0;
-
-    std::array<char_type, 256> m_edges = {};
-    std::size_t m_num_edges = 0;
-
-  public:
-    constexpr builder(const Words& words) : m_words(words) {
-        arrange(0, std::size(words), 0, 0);
-    }
-    constexpr Units steal_units() {
-        return std::move(m_units);
-    }
-
-  private:
-    constexpr void arrange(std::size_t bpos, std::size_t epos, std::size_t depth, std::size_t npos) {
-        if (std::size(m_words[bpos]) == depth) {
-            m_units[npos].set_base(m_id++);
-            return;
-        }
-
-        m_num_edges = 0;
-        {
-            auto c1 = m_words[bpos][depth];
-            for (auto i = bpos + 1; i < epos; i++) {
-                const auto c2 = m_words[i][depth];
-                if (c1 != c2) {
-                    m_edges[m_num_edges++] = c1;
-                    c1 = c2;
-                }
-            }
-            m_edges[m_num_edges++] = c1;
-        }
-
-        const auto base = xcheck();
-        if (base == std::numeric_limits<std::size_t>::max()) {
-            throw std::logic_error("The capacity is not enough.");
-        }
-
-        m_units[npos].set_base(static_cast<int_type>(base));
-
-        for (std::size_t i = 0; i < m_num_edges; i++) {
-            const auto cpos = base ^ static_cast<std::size_t>(m_edges[i]);
-            m_units[cpos].set_check(static_cast<int_type>(npos));
-        }
-
-        auto i = bpos;
-        auto c1 = m_words[bpos][depth];
-
-        for (auto j = bpos + 1; j < epos; j++) {
-            const auto c2 = m_words[j][depth];
-            if (c1 != c2) {
-                arrange(i, j, depth + 1, base ^ static_cast<std::size_t>(c1));
-                i = j;
-                c1 = c2;
-            }
-        }
-        arrange(i, epos, depth + 1, base ^ static_cast<std::size_t>(c1));
-    }
-
-    constexpr std::size_t xcheck() {
-        for (std::size_t base = 0; base < m_units.size(); base++) {
-            if (is_target(base)) {
-                return base;
-            }
-        }
-        return std::numeric_limits<std::size_t>::max();
-    }
-
-    constexpr bool is_target(const std::size_t base) {
-        for (std::size_t i = 0; i < m_num_edges; i++) {
-            const auto npos = base ^ std::size_t(m_edges[i]);
-            if (m_units[npos].get_check() >= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
-}  // namespace details
-
-namespace utils {
 
 constexpr std::size_t get_num_words(const std::string_view& text) {
     if (text.back() != '\0') {
@@ -182,7 +69,7 @@ constexpr auto text_to_words(const std::string_view& text) {
     std::array<std::string_view, N> words = {};
     auto b_itr = std::begin(text);
     for (std::size_t i = 0; i < N; i++) {
-        auto e_itr = details::find(b_itr, std::end(text), '\0') + 1;
+        auto e_itr = find(b_itr, std::end(text), '\0') + 1;
         words[i] = text.substr(std::distance(std::begin(text), b_itr), std::distance(b_itr, e_itr));
         b_itr = e_itr;
     }
@@ -191,6 +78,179 @@ constexpr auto text_to_words(const std::string_view& text) {
 
 }  // namespace utils
 
+namespace details {
+
+template <class Words, class Units>
+class builder {
+  public:
+    using int_type = typename Units::value_type::int_type;
+    using char_type = typename Words::value_type::value_type;
+
+    Units m_units;
+
+  private:
+    const Words& m_words;
+
+    std::size_t m_id = 0;
+    std::size_t m_size = 1;
+
+    std::array<char_type, 256> m_edges = {};
+    std::size_t m_num_edges = 0;
+
+  public:
+    constexpr builder(const Words& words) : m_words(words) {
+        init();
+        arrange(0, std::size(words), 0, 0);
+        m_units[0].set_check((m_size + 255) / 256 * 256);  // set the occupied size
+    }
+
+  private:
+    constexpr void init() {
+        const auto size = static_cast<int_type>(std::size(m_units));
+        for (int_type i = 1; i < size; i++) {
+            m_units[i].set_base(-1 * (i + 1));
+            m_units[i].set_check(-1 * (i - 1));
+        }
+        m_units[size - 1].set_base(-1);
+        m_units[1].set_check(-1 * (size - 1));
+        m_units[0].set_check(1);  // Empty head
+    }
+
+    constexpr void fix_child(std::size_t npos, std::size_t cpos) {
+        assert(m_units[npos].get_check() >= 0);
+        assert(m_units[cpos].get_check() < 0);
+
+        const auto next = -1 * m_units[cpos].get_base();
+        const auto prev = -1 * m_units[cpos].get_check();
+        m_units[prev].set_base(-1 * next);
+        m_units[next].set_check(-1 * prev);
+
+        // Update empty head
+        if (cpos == m_units[0].get_check()) {
+            if (next == cpos) {  // No empty units?
+                m_units[0].set_check(0);
+            } else {
+                m_units[0].set_check(next);
+            }
+        }
+
+        m_units[cpos].set_check(static_cast<int_type>(npos));
+        m_size = std::max(m_size, cpos + 1);
+    }
+
+    constexpr void arrange(std::size_t bpos, std::size_t epos, std::size_t depth, std::size_t npos) {
+        if (std::size(m_words[bpos]) == depth) {
+            if (m_words[bpos].back() != '\0') {
+                throw std::logic_error("An input word has to be terminated by NULL character.");
+            }
+            assert(m_units[npos].get_base() < 0);
+            m_units[npos].set_base(m_id++);
+            return;
+        }
+
+        m_num_edges = 0;
+        {
+            auto c1 = m_words[bpos][depth];
+            for (auto i = bpos + 1; i < epos; i++) {
+                const auto c2 = m_words[i][depth];
+                if (c1 != c2) {
+                    if (c1 > c2) {
+                        throw std::logic_error("The input data is not in lexicographical order.");
+                    }
+                    m_edges[m_num_edges++] = c1;
+                    c1 = c2;
+                }
+            }
+            m_edges[m_num_edges++] = c1;
+        }
+
+        const auto base = xcheck();
+        if (base == std::numeric_limits<std::size_t>::max()) {
+            throw std::logic_error("The capacity is not enough. Increase RESERVE_FACTOR.");
+        }
+
+        assert(m_units[npos].get_base() < 0);
+        m_units[npos].set_base(static_cast<int_type>(base));
+
+        for (std::size_t i = 0; i < m_num_edges; i++) {
+            fix_child(npos, base ^ static_cast<std::size_t>(m_edges[i]));
+        }
+
+        auto i = bpos;
+        auto c1 = m_words[bpos][depth];
+
+        for (auto j = bpos + 1; j < epos; j++) {
+            const auto c2 = m_words[j][depth];
+            if (c1 != c2) {
+                arrange(i, j, depth + 1, base ^ static_cast<std::size_t>(c1));
+                i = j;
+                c1 = c2;
+            }
+        }
+        arrange(i, epos, depth + 1, base ^ static_cast<std::size_t>(c1));
+    }
+
+    constexpr std::size_t xcheck() {
+        assert(m_num_edges != 0);
+
+        const std::size_t epos = m_units[0].get_check();
+        if (epos == 0) {
+            return std::numeric_limits<std::size_t>::max();
+        }
+
+        std::size_t npos = epos;
+        do {
+            const std::size_t base = npos ^ std::size_t(m_edges[0]);
+            if (is_target(base)) {
+                return base;
+            }
+            assert(m_units[npos].get_base() < 0);
+            npos = -1 * m_units[npos].get_base();
+        } while (npos != epos);
+
+        return std::numeric_limits<std::size_t>::max();
+    }
+
+    constexpr bool is_target(const std::size_t base) {
+        for (std::size_t i = 0; i < m_num_edges; i++) {
+            const auto npos = base ^ std::size_t(m_edges[i]);
+            if (m_units[npos].get_check() >= 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+template <class Words>
+constexpr void get_num_nodes(const Words& words, std::size_t& num_nodes,  //
+                             std::size_t bpos, std::size_t epos, std::size_t depth) {
+    if (std::size(words[bpos]) == depth) {
+        if (words[bpos].back() != '\0') {
+            throw std::logic_error("An input word has to be terminated by NULL character.");
+        }
+        return;
+    }
+
+    auto i = bpos;
+    auto c1 = words[bpos][depth];
+
+    for (auto j = bpos + 1; j < epos; j++) {
+        const auto c2 = words[j][depth];
+        if (c1 != c2) {
+            if (c1 > c2) {
+                throw std::logic_error("The input data is not in lexicographical order.");
+            }
+            get_num_nodes(words, ++num_nodes, i, j, depth + 1);
+            i = j;
+            c1 = c2;
+        }
+    }
+    get_num_nodes(words, ++num_nodes, i, epos, depth + 1);
+}
+
+}  // namespace details
+
 template <class Words>
 constexpr std::size_t get_capacity(const Words& words) {
     std::size_t num_nodes = 0;
@@ -198,28 +258,42 @@ constexpr std::size_t get_capacity(const Words& words) {
     return (static_cast<std::size_t>(num_nodes * RESERVE_FACTOR) + 255) / 256 * 256;
 }
 
-template <std::size_t N, class Words, class Unit = unit<int>>
-constexpr auto make(const Words& words) {
-    details::builder<Words, std::array<Unit, N>> b(words);
-    return b.steal_units();
+template <class Units>
+constexpr std::size_t get_size(const Units& units) {
+    return units[0].get_check();
+}
+
+template <std::size_t Capacity, class Words, class Unit = unit<int>>
+constexpr std::array<Unit, Capacity> make(const Words& words) {
+    details::builder<Words, std::array<Unit, Capacity>> b(words);
+    return std::move(b.m_units);  // maybe, the move does not make sense
+}
+
+template <std::size_t Size, std::size_t Capacity, class Unit = unit<int>>
+constexpr std::array<Unit, Size> shrink_to_fit(const std::array<Unit, Capacity>& units) {
+    std::array<Unit, Size> new_units = {};
+    for (std::size_t i = 0; i < Size; i++) {
+        new_units[i] = units[i];
+    }
+    return new_units;
 }
 
 template <class Units, class Word>
-constexpr std::size_t search(const Units& units, const Word& word) {
+constexpr std::tuple<std::size_t, std::size_t> search(const Units& units, const Word& word) {
     std::size_t npos = 0;
     for (const auto c : word) {
         const std::size_t cpos = units[npos].get_base() ^ static_cast<std::size_t>(c);
         if (units[cpos].get_check() != npos) {
-            return std::numeric_limits<std::size_t>::max();
+            return {npos, std::numeric_limits<std::size_t>::max()};
         }
         npos = cpos;
     }
 
-    const std::size_t cpos = units[npos].get_base();  // ^ '\0'
+    const std::size_t cpos = units[npos].get_base();  // ^'\0'
     if (units[cpos].get_check() != npos) {
-        return std::numeric_limits<std::size_t>::max();
+        return {npos, std::numeric_limits<std::size_t>::max()};
     } else {
-        return static_cast<std::size_t>(units[npos].get_base());
+        return {cpos, static_cast<std::size_t>(units[cpos].get_base())};
     }
 }
 
--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,9 @@
 
 using namespace std::string_view_literals;
 
-static constexpr auto text = "abc\0abd\0bcc\0ccsa\0"sv;
+static constexpr std::array<std::string_view, 5> words = {
+    "ICDE\0"sv, "ICDM\0"sv, "ICML\0"sv, "SIGIR\0"sv, "SIGMOD\0"sv,
+};
 
 std::ostream& operator<<(std::ostream& os, const std::tuple<std::size_t, std::size_t>& v) {
     os << std::get<0>(v) << ',' << std::get<1>(v);
@@ -12,8 +14,6 @@ std::ostream& operator<<(std::ostream& os, const std::tuple<std::size_t, std::si
 }
 
 constexpr auto make_units() {
-    constexpr auto num_words = constexpr_doublearray::utils::get_num_words(text);
-    constexpr auto words = constexpr_doublearray::utils::text_to_words<num_words>(text);
     constexpr auto capacity = constexpr_doublearray::get_capacity(words);
     constexpr auto units = constexpr_doublearray::make<capacity>(words);
     constexpr auto size = constexpr_doublearray::get_size(units);
@@ -23,11 +23,7 @@ constexpr auto make_units() {
 int main() {
     constexpr auto units = make_units();
 
-    std::cout << constexpr_doublearray::search(units, "abc"sv) << std::endl;
-    std::cout << constexpr_doublearray::search(units, "abd"sv) << std::endl;
-    std::cout << constexpr_doublearray::search(units, "bcc"sv) << std::endl;
-    std::cout << constexpr_doublearray::search(units, "ccsa"sv) << std::endl;
-    std::cout << constexpr_doublearray::search(units, "ab"sv) << std::endl;
+    std::cout << constexpr_doublearray::search(units, "ICDE"sv) << std::endl;
 
     return 0;
 }
